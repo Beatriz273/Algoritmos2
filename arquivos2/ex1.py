e2bdc67e9020a7c7f93954d974791fdbbb9b062e
Beatriz Ferreira <124069060+Beatriz273@users.noreply.github.com>
2023-10-31 14:03:46 +0000

    a

--- arquivos2/ex1.py
+++ arquivos2/ex1.py
@@ -1,18 +1,35 @@
 usuario = open("arquivos2/usuarios.txt", "w")
-qntUsuarios = 1
+qntUsuarios = 2
 espacoutilizado = []
 nome = []
 id = []
-capacidadetotal = 2581.54
 porcentagem = []
+espacomedio = []
+capacidadetotal = []
 
+def nomeeID():
+    for i in range(qntUsuarios):
+        nome.append(input(f"Insira o nome do usuário: "))
+        id.append(int(input(f"Insira o ID do usuário: ")))
+        usuario.write(f"Nome do usuário {nome[i]}, e ID {id[i]} \n")
+    
+def espaco():
+    for i in range(qntUsuarios):
+        espacoutilizado.append(int(input("Insira o espaço utilizado em bytes: ")))
+        espacoutilizado[i] = (espacoutilizado[i] / 1000024)
+        capacidadetotal = []
+        capacidadetotal += espacoutilizado[i]
 
-for i in range(qntUsuarios):
-    nome.append(input(f"Insira o nome do usuário: "))
-    id.append(int(input(f"Insira o ID do usuário: ")))
-    espacoutilizado.append(float(input("Insira o espaço utilizado em bytes: ")))
-    usuario.write(f"Nome do usuário {nome[i]}, e ID {id[i]} \n")
-    espacoutilizado.append(espacoutilizado[i]//1000024)
-    porcentagem = (espacoutilizado/capacidadetotal) * 100
+def porcentagemusuarios():
+    for i in range(qntUsuarios):
+        porcentagem.append((espacoutilizado[i]/capacidadetotal[i]) * 100)
 
 relatorio = open("arquivos2/relatorio.txt", "w")
+
+nomeeID()
+espaco()
+porcentagemusuarios()
+
+for i in range(qntUsuarios):
+    relatorio.write(f"O {i+1}º usuário {nome[i]} utiliza cerca de {espacoutilizado[i]}MB. Cerca de {porcentagem[i]:.2f}% da capacidade total.")
+    relatorio.write(f"Espaço total ocupado: {capacidadetotal}")
