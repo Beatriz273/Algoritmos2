8deb18262fb214092d17ea8f9bcfee000ae0bef8
Beatriz Ferreira <124069060+Beatriz273@users.noreply.github.com>
2023-11-07 13:55:47 +0000

    Atividade

--- Trabalho final/inicial.py
+++ Trabalho final/inicial.py
@@ -2,4 +2,4 @@ coluna = []
 
 with open("Relacao_de_Empregados_CLT.csv", "r") as CLT, open ("Relacao_de_Ocupantes_CC___FG2.csv", "r") as Relacao, open ("linhas.csv", "w"):
     for linha in CLT:
-        coluna = linha.split(;)
+        coluna = linha.split(";")
